84e4bf21cd62e68fa4e3336b8fae3e3cd27ee686
Saksham1611 <guptasaksham1611@gmail.com>
2021-10-02 15:57:44 +0530

    add data path location in config

---
+++ src/config.py
@@ -0,0 +1,9 @@
+#path of all the data  
+DATAPATH=                           "../input/"
+TRAIN_DATA=                DATAPATH + "train_data.csv"
+TEST_DATA=                 DATAPATH + "test_data.csv"
+CAMPAIGN_DATA=             DATAPATH + "otherdata/campaign_data.csv"
+COUPON_ITEM_DATA=          DATAPATH + "otherdata/coupon_item_mapping.csv"
+CUSTOMER_DEMOGRAPHICS_DATA=DATAPATH + "otherdata/customer_demographics.csv"
+CUSTOMER_TRANSACTION_DATA= DATAPATH + "otherdata/customer_transaction_data.csv"
+ITEM_DATA=                 DATAPATH + "otherdata/item_data.csv"
