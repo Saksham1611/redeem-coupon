aff7201e5ae35f6721567901489b0849202bd6b4
Saksham1611 <guptasaksham1611@gmail.com>
2021-10-13 23:06:54 +0530

    add app and predict file

--- app.py
+++ app.py
@@ -2,35 +2,34 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import joblib
 import numpy as np
-from config import MODEL_PATH_LOGRES ,MODEL_PATH_XGBOSST 
 
-APP = Flask(__name__)
-API = Api(APP)
+
+app = Flask(__name__)
+api = Api(app)
 
 #change the model type to experiment around
-logres_model = joblib.load(MODEL_PATH_LOGRES)
+logres_model = joblib.load("models/logres_model.pkl")
 
 class Predict(Resource):
 
     @staticmethod
     def post():
         parser = reqparse.RequestParser()
-        # parser.add_argument('id')
-        # parser.add_argument('petal_width')
-        # parser.add_argument('sepal_length')
-        # parser.add_argument('sepal_width')
+        parser.add_argument('campaign_id')
+        parser.add_argument('coupon_id')
+        parser.add_argument('customer_id')
 
         args = parser.parse_args()  # creates dict
 
-        X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
+        X_new = np.fromiter(args.values(), dtype=str)  # convert input to array
 
-        out = {'Prediction': logres_model.predict([X_new])[0]}
+        out = {'redemption_status': logres_model.predict([X_new])[0]}
 
         return out, 200
         
 
-API.add_resource(Predict, '/predict')
+api.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-    APP.run(debug=True, port='1080')
+    app.run(debug=True,port=1088)
 
